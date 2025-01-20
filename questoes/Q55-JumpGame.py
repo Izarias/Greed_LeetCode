ebbeffa7b7ad8d778bec698b0a8f412116e5e398
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2025-01-20 16:04:42 -0300

    Update Q55-JumpGame.py

--- questoes/Q55-JumpGame.py
+++ questoes/Q55-JumpGame.py
@@ -2,7 +2,10 @@ class Solution:
     def canJump(self, nums):
         max_reach = 0
         for i in range(len(nums)):
-            if i > max_reach: 
+            if i > max_reach:
                 return False
-            max_reach = max(max_reach, i + nums[i]) 
-        return max_reach >= len(nums) - 1 
+            max_reach = max(max_reach, i + nums[i])
+            # Se já alcançamos o último índice, podemos parar.
+            if max_reach >= len(nums) - 1:
+                return True
+        return False
